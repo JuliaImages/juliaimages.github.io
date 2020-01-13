c39b33e0b6af3b26c8a86a748a76647b3d9cdaa9
Archit Rungta <architrungta120@gmail.com>
2020-01-13 15:17:54 +0530

    Add comments

--- docs/examples/contours/contour_detection.jl
+++ docs/examples/contours/contour_detection.jl
@@ -4,20 +4,29 @@
 # ---
 
 # This demonstration shows how to detect contours on binary images
-# the algorithm implemented is exactly same as OpenCV
+# The algorithm used is ["Topological Structural Analysis of Digitized Binary Images
+# by Border Following"](https://www.sciencedirect.com/science/article/pii/0734189X85900167) by Suzuki and Abe (Same as OpenCV).#
+
+# Points are represented using 2-int arrays. Contours are represented as a vector of Points.
+# Direction is a single number between 1 to 8. Steps inside functions are marked as they are in the original paper
 
 using Images, TestImages, FileIO
-dir_delta = [[-1, 0] , [-1, 1], [0, 1], [1, 1], [1, 0], [1, -1], [0, -1], [-1,-1]]
 
+dir_delta = [[-1, 0] , [-1, 1], [0, 1], [1, 1], [1, 0], [1, -1], [0, -1], [-1,-1]]
+##              N          NE      E       SE      S       SW        W      NW
+## direction between two pixels
+
+## rotate direction clocwise
 function clockwise(dir)
     return (dir)%8 + 1
 end
 
+## rotate direction counterclocwise
 function counterclockwise(dir)
     return (dir+6)%8 + 1
 end
 
-
+## move from current pixel to next in given direction
 function move(pixel, image, dir)
     newp = pixel + dir_delta[dir]
     if newp[1] > 0 && newp[1] <= size(image, 1) && newp[2] > 0 && newp[2] <= size(image, 2)
@@ -28,7 +37,7 @@ function move(pixel, image, dir)
     return [0,0]
 end
 
-
+## finds direction between two given pixels
 function from_to(from, to)
     delta = to-from
     return findall(x->x == delta, dir_delta)[1]
@@ -40,7 +49,7 @@ function detect_move(image, p0, p2, nbd, border)
     dir = from_to(p0, p2)
     moved = clockwise(dir)
     p1 = [0, 0]
-    while moved != dir
+    while moved != dir ## 3.1
         newp = move(p0, image, moved)
         if newp[1]!=0
             p1 = newp
@@ -53,16 +62,15 @@ function detect_move(image, p0, p2, nbd, border)
         return
     end
 
-    p2 = p1
-    p3 = p0
-
+    p2 = p1 ## 3.2
+    p3 = p0 ## 3.2
     done = [false, false, false, false, false, false, false, false]
     while true
         dir = from_to(p3, p2)
         moved = counterclockwise(dir)
         p4 = [0,0]
         done = [false, false, false, false, false, false, false, false]
-        while true
+        while true ## 3.3
             p4 = move(p3, image, moved)
             if p4[1] != 0
                 break
@@ -70,20 +78,21 @@ function detect_move(image, p0, p2, nbd, border)
             done[moved] = true
             moved = counterclockwise(moved)
         end
-        push!(border, p3)
+        push!(border, p3) ## 3.4
         if image[p3...] != 0 && (p3[1] == size(image, 1) || done[5])
             image[p3...] = -nbd
         elseif image[p3...] == 1
             image[p3...] = nbd
         end
 
-        if (p4 == p0 && p3 == p1)
+        if (p4 == p0 && p3 == p1) ## 3.5
             break
         end
         p2 = p3
         p3 = p4
     end
 end
+
 
 function find_contours(image)
     nbd = 1
@@ -95,9 +104,10 @@ function find_contours(image)
         lnbd = 1
         for j=1:size(image, 2)
             fji = image[i, j]
-            is_outer = (image[i, j] == 1 && (j == 1 || image[i, j-1] == 0))
-            is_hole = (image[i, j] == 1 && (j == size(image, 2) || image[i, j+1] == 0))
+            is_outer = (image[i, j] == 1 && (j == 1 || image[i, j-1] == 0)) ## 1 (a)
+            is_hole = (image[i, j] >= 1 && (j == size(image, 2) || image[i, j+1] == 0)) ## 1(b)
             if is_outer || is_hole
+                ## 2
                 border = Array{Int64}[]
                 from = [i, j]
                 if is_outer
@@ -112,7 +122,7 @@ function find_contours(image)
                 end
 
                 p0 = [i,j]
-                detect_move(image, p0, from, nbd, border)
+                detect_move(image, p0, from, nbd, border) ## 3
                 if size(border)==0
                     append!(border, p0)
                     image[p0...] = -nbd
@@ -133,6 +143,7 @@ function find_contours(image)
 
 end
 
+## a contour is a vector of 2 int arrays
 function draw_contour(image, color, contour)
     for ind in contour
         image[ind...] = color
@@ -144,21 +155,26 @@ function draw_contours(image, color, contours)
     end
 end
 
+## load images
 img1 = testimage("mandrill")
 img2 = testimage("lighthouse")
 
+## convert to grayscale
 imgg1 = Gray.(img1)
 imgg2 = Gray.(img2)
 
+## threshold
 imgg1 = imgg1 .> 0.45
 imgg2 = imgg2 .> 0.45
 
+## calling find_contours
 cnts1 = find_contours(imgg1)
 cnts2 = find_contours(imgg2)
 
 img3 = copy(img1)
 img4 = copy(img2)
 
+## finally, draw the detected contours
 draw_contours(img3, RGB(1,0,0), cnts1)
 draw_contours(img4, RGB(1,0,0), cnts2)
 
