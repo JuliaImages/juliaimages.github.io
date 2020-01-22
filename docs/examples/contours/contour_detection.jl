cc7b82ce9644fcc99742628afb91805cffd71ed3
Archit Rungta <architrungta120@gmail.com>
2020-01-22 15:07:14 +0530

    Fix implementation and use cartesianindex

--- docs/examples/contours/contour_detection.jl
+++ docs/examples/contours/contour_detection.jl
@@ -7,12 +7,11 @@
 # The algorithm used is ["Topological Structural Analysis of Digitized Binary Images
 # by Border Following"](https://www.sciencedirect.com/science/article/pii/0734189X85900167) by Suzuki and Abe (Same as OpenCV).#
 
-# Points are represented using 2-int arrays. Contours are represented as a vector of Points.
+# Points are represented using CartesianIndex. Contours are represented as a vector of Points.
 # Direction is a single number between 1 to 8. Steps inside functions are marked as they are in the original paper
 
 using Images, TestImages, FileIO
 
-dir_delta = [[-1, 0] , [-1, 1], [0, 1], [1, 1], [1, 0], [1, -1], [0, -1], [-1,-1]]
 ##              N          NE      E       SE      S       SW        W      NW
 ## direction between two pixels
 
@@ -27,30 +26,31 @@ function counterclockwise(dir)
 end
 
 ## move from current pixel to next in given direction
-function move(pixel, image, dir)
+function move(pixel, image, dir, dir_delta)
     newp = pixel + dir_delta[dir]
-    if newp[1] > 0 && newp[1] <= size(image, 1) && newp[2] > 0 && newp[2] <= size(image, 2)
-        if image[newp...]!=0
+    height, width = size(image)
+    if (0 < newp[1] <= height) &&  (0 < newp[2] <= width)
+        if image[newp]!=0
             return newp
         end
     end
-    return [0,0]
+    return CartesianIndex(0, 0)
 end
 
 ## finds direction between two given pixels
-function from_to(from, to)
+function from_to(from, to, dir_delta)
     delta = to-from
     return findall(x->x == delta, dir_delta)[1]
 end
 
 
 
-function detect_move(image, p0, p2, nbd, border)
-    dir = from_to(p0, p2)
+function detect_move(image, p0, p2, nbd, border, done, dir_delta)
+    dir = from_to(p0, p2, dir_delta)
     moved = clockwise(dir)
-    p1 = [0, 0]
+    p1 = CartesianIndex(0, 0)
     while moved != dir ## 3.1
-        newp = move(p0, image, moved)
+        newp = move(p0, image, moved, dir_delta)
         if newp[1]!=0
             p1 = newp
             break
@@ -58,20 +58,20 @@ function detect_move(image, p0, p2, nbd, border)
         moved = clockwise(moved)
     end
 
-    if p1 == [0,0]
+    if p1 == CartesianIndex(0, 0)
         return
     end
 
     p2 = p1 ## 3.2
     p3 = p0 ## 3.2
-    done = [false, false, false, false, false, false, false, false]
+    done .= false
     while true
-        dir = from_to(p3, p2)
+        dir = from_to(p3, p2, dir_delta)
         moved = counterclockwise(dir)
-        p4 = [0,0]
-        done = [false, false, false, false, false, false, false, false]
+        p4 = CartesianIndex(0, 0)
+        done .= false
         while true ## 3.3
-            p4 = move(p3, image, moved)
+            p4 = move(p3, image, moved, dir_delta)
             if p4[1] != 0
                 break
             end
@@ -79,10 +79,10 @@ function detect_move(image, p0, p2, nbd, border)
             moved = counterclockwise(moved)
         end
         push!(border, p3) ## 3.4
-        if image[p3...] != 0 && (p3[1] == size(image, 1) || done[5])
-            image[p3...] = -nbd
-        elseif image[p3...] == 1
-            image[p3...] = nbd
+        if p3[1] == size(image, 1) || done[3]
+            image[p3] = -nbd
+        elseif image[p3] == 1
+            image[p3] = nbd
         end
 
         if (p4 == p0 && p3 == p1) ## 3.5
@@ -98,36 +98,46 @@ function find_contours(image)
     nbd = 1
     lnbd = 1
     image = convert(Array{Float64}, image)
-    unique_map = Dict{typeof(Array{Int64}[]), Int64}()
+    contour_list =  Vector{typeof(CartesianIndex[])}()
+    done = [false, false, false, false, false, false, false, false]
 
-    for i=1:size(image, 1)
+    ## Clockwise Moore neighborhood.
+    dir_delta = [CartesianIndex(-1, 0) , CartesianIndex(-1, 1), CartesianIndex(0, 1), CartesianIndex(1, 1), CartesianIndex(1, 0), CartesianIndex(1, -1), CartesianIndex(0, -1), CartesianIndex(-1,-1)]
+
+    height, width = size(image)
+
+    for i=1:height
         lnbd = 1
-        for j=1:size(image, 2)
+        for j=1:width
             fji = image[i, j]
             is_outer = (image[i, j] == 1 && (j == 1 || image[i, j-1] == 0)) ## 1 (a)
-            is_hole = (image[i, j] >= 1 && (j == size(image, 2) || image[i, j+1] == 0)) ## 1(b)
+            is_hole = (image[i, j] >= 1 && (j == width || image[i, j+1] == 0))
+
             if is_outer || is_hole
                 ## 2
-                border = Array{Int64}[]
-                from = [i, j]
+                border = CartesianIndex[]
+
+                from = CartesianIndex(i, j)
+
                 if is_outer
                     nbd += 1
-                    from[1] -= 1
+                    from -= CartesianIndex(0, 1)
+
                 else
                     nbd += 1
                     if fji > 1
                         lnbd = fji
                     end
-                    from[1] += 1
+                    from += CartesianIndex(0, 1)
                 end
 
-                p0 = [i,j]
-                detect_move(image, p0, from, nbd, border) ## 3
-                if size(border)==0
-                    append!(border, p0)
-                    image[p0...] = -nbd
+                p0 = CartesianIndex(i,j)
+                detect_move(image, p0, from, nbd, border, done, dir_delta) ## 3
+                if isempty(border) ##TODO
+                    push!(border, p0)
+                    image[p0] = -nbd
                 end
-                unique_map[sort(border)]=1
+                push!(contour_list, border)
             end
             if fji != 0 && fji != 1
                 lnbd = abs(fji)
@@ -135,18 +145,16 @@ function find_contours(image)
 
         end
     end
-    contour_list =  Vector{typeof(Array{Int64}[])}()
-    for (k, v) in unique_map
-        push!(contour_list, k)
-    end
+
     return contour_list
+
 
 end
 
 ## a contour is a vector of 2 int arrays
 function draw_contour(image, color, contour)
     for ind in contour
-        image[ind...] = color
+        image[ind] = color
     end
 end
 function draw_contours(image, color, contours)
