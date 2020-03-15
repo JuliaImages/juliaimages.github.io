2dad4869d4fa4d0d70ca5b5c63e33ba8966a0ebb
zeptodoctor <44736852+zeptodoctor@users.noreply.github.com>
2020-03-15 20:48:47 +0000

    build based on 6e1e3ba

--- previews/PR116/democards/examples/spatial_transformation/image_diffview.jl
+++ previews/PR116/democards/examples/spatial_transformation/image_diffview.jl
@@ -5,25 +5,21 @@ img = float.(testimage("cameraman"))
 # rotate img by 4 degrees and keep axes unchanged
 img_r = imrotate(img, -pi/45, axes(img))
 
-# The simplest version is a plain diff
+mosaicview(img, img_r; nrow=1, npad=20, fillvalue=colorant"white")
+
 plain_diffview = @. img - img_r
 
-# For gray images, a fancy trick is to fill each image into different RGB channels
-RGB_diffview = colorview(RGB,
-    PermutedDimsArray(
-    cat(channelview(img), channelview(img_r), fill(0., size(img)),; dims=3),
-    (3, 1, 2))
-)
+rgb_data = cat(channelview(img),
+               channelview(img_r),
+               fill(0., size(img)); # empty fill
+               dims=3) # size: (512, 512, 3)
+rgb_data = PermutedDimsArray(rgb_data, (3, 1, 2)) # size: (3, 512, 512)
+RGB_diffview = colorview(RGB, rgb_data)
 
-# or convert the RGB view back to Gray after that
 Gray_diffview = Gray.(RGB_diffview)
 
 mosaicview(plain_diffview, RGB_diffview, Gray_diffview;
            nrow=1, npad=20, fillvalue=colorant"white")
-
-
-using FileIO # src
-save("assets/image_diffview.png", RGB_diffview) # src
 
 # This file was generated using Literate.jl, https://github.com/fredrikekre/Literate.jl
 
