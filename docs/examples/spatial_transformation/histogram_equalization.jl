18356b56e102c4b7d97cfbf3f9947536244a1c4a
Frank-Schmitt <frank.schmitt@gmail.com>
2021-01-06 16:06:56 +0100

    Fix typos (#176)

--- docs/examples/spatial_transformation/histogram_equalization.jl
+++ docs/examples/spatial_transformation/histogram_equalization.jl
@@ -5,12 +5,12 @@
 # date: 2020-01-12
 # ---
 
-# This demo issustrates the use of [Histogram equalization](https://juliaimages.org/ImageContrastAdjustment.jl/stable/reference/#Equalization-1), [gamma correction matching](https://juliaimages.org/ImageContrastAdjustment.jl/stable/reference/#Matching-1)
+# This demo illustrates the use of [Histogram equalization](https://juliaimages.org/ImageContrastAdjustment.jl/stable/reference/#Equalization-1), [gamma correction matching](https://juliaimages.org/ImageContrastAdjustment.jl/stable/reference/#Matching-1)
 # and [Contrast Limited Adaptive Histogram Equalization](https://juliaimages.org/ImageContrastAdjustment.jl/stable/reference/#AdaptiveEqualization-1)
 
-# Histogram equalisation is used to imporve the contrast in an
+# Histogram equalisation is used to improve the contrast in an
 # single-channel grayscale image. It distributes the intensity of the image
-# in a uniform manner. The natural justification for uniformity
+# in an uniform manner. The natural justification for uniformity
 # is that the image has better contrast if the intensity levels of an image span
 # a wide range on the intensity scale. The transformation is based on mapping of
 # cumulative histogram
