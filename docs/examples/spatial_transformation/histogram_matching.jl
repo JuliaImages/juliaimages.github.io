9eb8f834b4a88080f1e7e4b33d93e4d66074be56
Ashwani Rathee <ab669522@gmail.com>
2021-03-22 13:22:40 +0530

    rework demo

--- docs/examples/spatial_transformation/histogram_matching.jl
+++ docs/examples/spatial_transformation/histogram_matching.jl
@@ -10,12 +10,10 @@
 # This demo demonstrates the feature of histogram matching.
 
 
-# In histogram matching, the pixels of the source image is manipulated so that the histogram 
-# of reference image matches with source image's histogram.
+# Purpose of Histogram matching is to transform the intensities in a source image so that the intensities distribute according to the histogram of a specified target image. 
 
-
-# Here, histogram matching is shown for the grayscale image. But, it can also be applied on 
-# RGB images too. Histogram matching can be used to normalize two images when the images 
+# Here, histogram matching is shown for the RGB image. But, it can also be applied on 
+# Grayscale images too. Histogram matching can be used to normalize two images when the images 
 # were acquired at the same local illumination (such as shadows) over the same location,
 # but by different sensors, atmospheric conditions, or global illumination. 
 
@@ -28,16 +26,19 @@ using Images
 using Plots
 using TestImages
 
+# Load example images: one source image and one reference image 
 
-# Load example images: one source image and one reference image
-
+# "TestImages.jl" provides access to a good number of sample images for testing purposes. We first import test images chelsea and coffee which have
+# have been recently added to TestImages.jl 
 
 img_source = testimage("chelsea");
 img_reference = testimage("coffee");
 
-
-# Applying histogram matching on source image using the reference image
-
+# Applying histogram matching on source image using the reference image,
+# adjust_histogram function from ImageContrastAdjustment.jl 
+# returns a histogram matched image with a granularity of nbins i.e. number of bins. 
+# The first argument img is the image to be matched, and the second argument targetimg is 
+# the image having the desired histogram to be matched to.
 
 img_transformed = adjust_histogram(img_source, Matching(targetimg = img_reference));
 
@@ -46,14 +47,14 @@ img_transformed = adjust_histogram(img_source, Matching(targetimg = img_referenc
 
 
 mosaicview(img_source, img_reference, img_transformed; nrow = 1)
-save("assets/chelsea.gif",cat(img_source,img_transformed;dims=3)) #src
+save("assets/chelsea.gif", cat(img_source, img_transformed; dims = 3)) #src
 
 # From Left to Right: Source Image, Reference Image, Transformed Image
 
 
-# We want to treat our RGB image as 3-component vector,so that we 
-# can access data from red,green,channels to show
-# histogram matching effect.
+# We want to treat our RGB image as a 3-component vector, so that we 
+# can access data from red, green,channels to show
+# histogram matching effect through a combined graph.
 
 
 img_source = channelview(img_source);
@@ -61,63 +62,31 @@ img_reference = channelview(img_reference);
 img_transformed = channelview(img_transformed);
 
 
-# We plot for each RGB channel, the histogram, and the cumulative histogram. 
+# To show the effect of histogram matching, We plot for each RGB channel, the histogram, and the cumulative histogram.
 
+# Visual inspection from the plots confirms that img_transformed resembles img_target
+# much more closely than img_source. We plot the graph with Plots.jl by looping over the 
+# color channels and plotting them for each i.e. source image, reference image. transformed image
 
-# Clearly, the matched image has the same cumulative histogram as the 
-# reference image for each channel.
+# Analysing change of data in color channels: Red, Green, Blue
 
-
-# Analysing change of data in color channel red
-
-
-hist_sred = histogram(vec(img_source[1, :, :]));
-hist_rred = histogram(vec(img_reference[1, :, :]));
-hist_tred = histogram(vec(img_transformed[1, :, :]));
+hist_final = []
+for i = 1:3
+    hist_s = histogram(vec(img_source[i, :, :]))
+    hist_r = histogram(vec(img_reference[i, :, :]))
+    hist_t = histogram(vec(img_transformed[i, :, :]))
+    push!(hist_final, hist_s)
+    push!(hist_final, hist_r)
+    push!(hist_final, hist_t)
+end
 plot(
-    hist_sred,
-    hist_rred,
-    hist_tred,
-    layout = (1, 3),
-    size = (1000, 333),
+    hist_final...,
+    layout = (3, 3),
+    size = (1000, 1000),
     legend = false,
-    title = ["source" "reference" "histograms matched"],
+    title = ["Source" "Reference" "Histograms Matched"],
+    reuse = false,
 )
-
-
-# Analysing change of data in color channel green
-
-
-hist_sgreen = histogram(vec(img_source[2, :, :]));
-hist_rgreen = histogram(vec(img_reference[2, :, :]));
-hist_tgreen = histogram(vec(img_transformed[2, :, :]));
-plot(
-    hist_sgreen,
-    hist_rgreen,
-    hist_tgreen,
-    layout = (1, 3),
-    size = (1000, 333),
-    legend = false,
-    title = ["source" "reference" "histograms matched"],
-)
-
-
-# Analysing change of data in color channel blue
-
-
-hist_sblue = histogram(vec(img_source[3, :, :]));
-hist_rblue = histogram(vec(img_reference[3, :, :]));
-hist_tblue = histogram(vec(img_transformed[3, :, :]));
-plot(
-    hist_sblue,
-    hist_rblue,
-    hist_tblue,
-    layout = (1, 3),
-    size = (1000, 333),
-    legend = false,
-    title = ["source" "reference" "histograms matched"],
-)
-
 
 # ### References:
 # 1. Python Example Implementation of Histogram Matching: https://scikit-image.org/docs/stable/auto_examples/color_exposure/plot_histogram_matching.html#sphx-glr-auto-examples-color-exposure-plot-histogram-matching-py
