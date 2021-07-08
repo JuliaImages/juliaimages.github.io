369915bb3f818dacf44cb9555861dabd52a3a5e6
Ashwani Rathee <ab669522@gmail.com>
2021-07-08 14:45:57 +0530

    addition of extra example

--- docs/examples/image_binarization/image_binarization.jl
+++ docs/examples/image_binarization/image_binarization.jl
@@ -16,7 +16,7 @@
 
 # Let's first import our sudoku
 
-using ImageBinarization, TestImages, MosaicViews, Colors
+using ImageBinarization, TestImages, MosaicViews, Colors, FileIO, ImageShow
 using ImageTransformations, CoordinateTransformations, Rotations, ImageMorphology
 
 # Original Image
@@ -28,18 +28,19 @@ img = testimage("sudoku")
 img = Gray.(img)
 
 # Let's first rotate our image
-trfm = recenter(RotMatrix(pi/30), center(img));
+
+trfm = recenter(RotMatrix(pi / 30), center(img));
 imgw = warp(img, trfm)
 
 # Now lets zoom in a bit
 
-imgw = parent(imgw)[65:490,65:490]
+imgw = parent(imgw)[65:490, 65:490]
 
 # Now let's binarize a image using Sauvola algorithm
 
 alg = Sauvola();
 img_otsu = binarize(imgw, alg)
-[imgw img_otsu]
+mosaicview(imgw, img_otsu; ncol = 2)
 
 # Now the differences between the binarized and non-binarized images 
 # become apparent. Let's now implement a function to see how different algorithms 
@@ -69,8 +70,41 @@ function binarize_methods(img_input, algs)
     return imgs_binarized
 end
 
-output = binarize_methods(imgw, algs)
-mosaicview(output, nrow = 3, npad = 1)
+output = binarize_methods(imgw, algs);
 
 
+for i = 1:12 #src
+    save("assets/test$(i).png", RGB.(output[i])) #src
+end #src
 
+# | Algorithm | Algorithm | Algorithm |
+# | :------: | :------: | :------: |
+# | AdaptiveThreshold | Balanced | Entropy |
+# | ![](assets/test1.png)  | ![](assets/test2.png)  | ![](assets/test3.png) |
+# | MinimumError |  Moments | Niblack |
+# | ![](assets/test5.png) | ![](assets/test6.png) | ![](assets/test7.png) |
+# | Polysegment | Sauvola | UnimodalRosin |
+# | ![](assets/test9.png) | ![](assets/test10.png) | ![](assets/test11.png) |
+# | Intermodes | Otsu | Yen |
+# | ![](assets/test4.png) | ![](assets/test8.png) | ![](assets/test12.png) |
+
+
+# Now let's take a simple example to understand behavior of these algorithms
+
+img = Array(reshape(range(0, stop = 1, length = 4 * 10^4), 200, 200));
+output = binarize_methods(Gray.(img), algs);
+
+for i = 1:12 #src
+    save("assets/test$(i+12).png", RGB.(output[i])) #src
+end #src
+
+# | Algorithm | Algorithm | Algorithm |
+# | :------: | :------: | :------: |
+# | AdaptiveThreshold | Balanced | Entropy |
+# | ![](assets/test113.png)  | ![](assets/test14.png)  | ![](assets/test15.png) |
+# | MinimumError |  Moments | Niblack |
+# | ![](assets/test16.png) | ![](assets/test17.png) | ![](assets/test18.png) |
+# | Polysegment | Sauvola | UnimodalRosin |
+# | ![](assets/test19.png) | ![](assets/test20.png) | ![](assets/test21.png) |
+# | Intermodes | Otsu | Yen |
+# | ![](assets/test22.png) | ![](assets/test23.png) | ![](assets/test24.png) |
