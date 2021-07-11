9d54d367a29d64fc51b316aa091c5be411a174a9
Ashwani Rathee <ab669522@gmail.com>
2021-07-11 09:43:00 +0530

    addition of content

--- docs/examples/image_binarization/image_binarization.jl
+++ docs/examples/image_binarization/image_binarization.jl
@@ -4,12 +4,12 @@
 # date: 2021-7-2
 # ---
 
-# ImageBinarization.jl provides number of algorithms for binarizing images
-# into background and foreground(bi-level image). In this demonstration , 
+# ImageBinarization.jl provides several algorithms for binarizing images
+# into background and foreground(bi-level image). In this demonstration, 
 # we'll be exploring where these algorithms could be useful and learn how
 # different algorithms are suited for different types of tasks.
 
-# Suppose a person wants to make a automatic Sudoku solver using Computer 
+# Suppose a person wants to make an automatic Sudoku solver using Computer 
 # Vision . Before anything else that person needs to import the image, 
 # preprocess it to optimize it for cell content extraction which can then 
 # be used for solving the sudoku. 
@@ -32,11 +32,11 @@ img = Gray.(img)
 trfm = recenter(RotMatrix(pi / 30), center(img));
 imgw = warp(img, trfm)
 
-# Now lets zoom in a bit
+# Now let's zoom in a bit
 
 imgw = parent(imgw)[65:490, 65:490]
 
-# Now let's binarize a image using Sauvola algorithm
+# Now let's binarize an image using the Sauvola algorithm
 
 alg = Sauvola();
 img_otsu = binarize(imgw, alg)
@@ -89,10 +89,15 @@ end #src
 # | ![](assets/test4.png) | ![](assets/test8.png) | ![](assets/test12.png) |
 
 
-# Now let's take a simple example to understand behavior of these algorithms
+# Now let's take a simple example to understand the behavior of these algorithms
+# 
 
-img = Array(reshape(range(0, stop = 1, length = 4 * 10^4), 200, 200));
-output = binarize_methods(Gray.(img), algs);
+img = Array(reshape(range(0, stop = 1, length = 4 * 10^4), 200, 200))
+img = Gray.(img)
+
+# Implementation of algorithms on this example
+
+output = binarize_methods(img, algs);
 
 for i = 1:12 #src
     save("assets/test$(i+12).png", RGB.(output[i])) #src
@@ -108,3 +113,13 @@ end #src
 # | ![](assets/test19.png) | ![](assets/test20.png) | ![](assets/test21.png) |
 # | Intermodes | Otsu | Yen |
 # | ![](assets/test22.png) | ![](assets/test23.png) | ![](assets/test24.png) |
+
+# For every pixel, the same threshold value is applied. In some cases, if the pixel value is smaller 
+# than the threshold, it is set to 0, otherwise it is set to a maximum value. In other algorithms,
+# a complete opposite approach is used.
+
+# Every algorithm has its own way of selecting that threshold and in some methods
+# it can be defined.
+
+# binarize_methods() could be particularly useful when you are not aware of the 
+# underlying assumptions made in algorithms and see all results and choose between them.
