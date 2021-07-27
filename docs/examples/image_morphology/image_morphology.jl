5f685e9fe3acfa2699835e51d97f5e005b964384
Ashwani Rathee <ab669522@gmail.com>
2021-07-27 21:27:46 +0530

    improved explanations, broadcasting fix

--- docs/examples/image_morphology/image_morphology.jl
+++ docs/examples/image_morphology/image_morphology.jl
@@ -5,12 +5,13 @@
 # date: 2021-7-2
 # ---
 
-# Morphological image processing is a area of computer vision which pursue tophat
-# remove imperfections from binary and grayscale images. ImageMorphology.jl
-# holds a collection of non-linear operations related to the shape or
-# morphology of features in an image.
+# Morphological image processing is an area of computer vision which pursues to
+# remove imperfections from binary and grayscale images.
 
-# In this demonstration, we cover the 6 most common morphological operations:
+# ImageMorphology.jl holds a collection of non-linear operations related to 
+# the shape or morphology of features in an image.
+
+# In this demonstration, we'll cover the 6 most common morphological operations:
 # - Erosion
 # - Dilation
 # - Opening
@@ -22,38 +23,51 @@
 
 # Let's import an image 
 
-using Images, ImageView, ImageMorphology, TestImages, MosaicViews
+using Images
+using ImageMorphology, TestImages
 
 img = testimage("morphology_test_512")
 
-# This is an RGB image, which we would need to be convert to either grayscale
+# This is an RGB image, which we would need to convert to either grayscale or
 # binary image as morphology functions only work on grayscale or binary images.
 
 # ## Erosion
 
-# Erode performs perform a min-filter over nearest-neighbors. The default 
+# Erode performs a min-filter over its nearest-neighbors. The default 
 # is 8-connectivity in 2d, 27-connectivity in 3d, etc. You can specify 
 # the list of dimensions that you want to include in the connectivity,
 #  e.g., `region = [1,2]` would exclude the third dimension from filtering.
 
+# Below example shows how erosion works on a binary image using `3x3`
+# structuring element. The `3x3` size structuring element checks neighbours
+# for every pixel in the original image to update that pixel's value with
+# `minimum` of the neighbours in the resultant image .
+
+# ![](assets/erodbin.png)
+
 img_erode = Gray.(Gray.(img) .< 0.8); # keeps white objects white
 img_erosion1 = erode(img_erode)
 img_erosion2 = erode(erode(img_erode))
-img_erosion3 = erode(erode(erode(img_erode)))
-mosaicview(img_erode, img_erosion1, img_erosion2, img_erosion3; nrow = 1)
+mosaicview(img_erode, img_erosion1, img_erosion2; nrow = 1)
 
 # ## Dilation
 
-# Dilate performs a max-filter over nearest-neighbors. The default is 
+# Dilate performs a max-filter over its nearest-neighbors. The default is 
 # 8-connectivity in 2d, 27-connectivity in 3d, etc. You can specify the
 # list of dimensions that you want to include in the connectivity,
 #  e.g., `region = [1,2]` would exclude the third dimension from filtering.
 
-img_dilate = Gray.(1 * Gray.(img) .> 0.9);
+# Below example shows how dilation works on a binary image using `3x3`
+# structuring element. The `3x3` size structuring element checks neighbours
+# for every pixel in the original image to update that pixel's value with
+# `maximum` of the neighbours in the resultant image .
+
+# ![](assets/dilatebin.png)
+
+img_dilate = Gray.(1 .* Gray.(img) .> 0.9);
 img_dilate1 = dilate(img_dilate)
 img_dilate2 = dilate(dilate(img_dilate))
-img_dilate3 = dilate(dilate(dilate(img_dilate)))
-mosaicview(img_dilate, img_dilate1, img_dilate2, img_dilate3; nrow = 1)
+mosaicview(img_dilate, img_dilate1, img_dilate2; nrow = 1)
 
 
 # ## Opening
@@ -63,11 +77,10 @@ mosaicview(img_dilate, img_dilate1, img_dilate2, img_dilate3; nrow = 1)
 # dimensions over which this operation is performed. Opening can remove 
 # small bright spots (i.e. “salt”) and connect small dark cracks.
 
-img_opening = Gray.(1 * Gray.(img) .> 0.5);
+img_opening = Gray.(1 .* Gray.(img) .> 0.5);
 img_opening1 = opening(img_opening)
 img_opening2 = opening(opening(img_opening))
-img_opening3 = opening(opening(opening(img_opening)))
-mosaicview(img_opening, img_opening1, img_opening2, img_opening3; nrow = 1)
+mosaicview(img_opening, img_opening1, img_opening2; nrow = 1)
 
 # ## Closing
 
@@ -76,11 +89,10 @@ mosaicview(img_opening, img_opening1, img_opening2, img_opening3; nrow = 1)
 # over which this operation is performed. Closing can remove small dark
 # spots (i.e. “pepper”) and connect small bright cracks.
 
-img_closing = Gray.(1 * Gray.(img) .> 0.5);
+img_closing = Gray.(1 .* Gray.(img) .> 0.5);
 img_closing1 = closing(img_closing)
 img_closing2 = closing(closing(img_closing))
-img_closing3 = closing(closing(closing(img_closing)))
-mosaicview(img_closing1, img_closing1, img_closing2, img_closing3; nrow = 1)
+mosaicview(img_closing1, img_closing1, img_closing2; nrow = 1)
 
 # ## Tophat
 
@@ -89,20 +101,20 @@ mosaicview(img_closing1, img_closing1, img_closing2, img_closing3; nrow = 1)
 # over which this operation is performed. This operation returns the bright
 # spots of the image that are smaller than the structuring element.
 
-img_tophat = Gray.(1 * Gray.(img) .> 0.2);
+img_tophat = Gray.(1 .* Gray.(img) .> 0.2);
 img_tophat1 = tophat(img_tophat)
 img_tophat2 = tophat(tophat(img_tophat))
 mosaicview(img_tophat, img_tophat1, img_tophat2; nrow = 1)
 
 # ## Bottom Hat
 
-# Bottom Hat morphology operation is defined as its morphological closing
+# Bottom Hat morphology operation is defined as image's morphological closing
 # minus the original image.
 # In `bothat(img, [region])`, `region` allows you to control the dimensions
 # over which this operation is performed.  This operation returns the 
 # dark spots of the image that are smaller than the structuring element.
 
-img_bothat = Gray.(1 * Gray.(img) .> 0.5);
+img_bothat = Gray.(1 .* Gray.(img) .> 0.5);
 img_bothat1 = bothat(img_tophat)
 img_bothat2 = bothat(bothat(img_tophat))
 img_bothat3 = bothat(bothat(bothat(img_tophat)))
@@ -114,7 +126,7 @@ mosaicview(img_bothat, img_bothat1, img_bothat2; nrow = 1)
 # given image. In `morphogradient(img, [region])`, `region` allows you to
 # control the dimensions over which this operation is performed.
 
-img_gray = Gray.(0.8 * Gray.(img) .> 0.7);
+img_gray = Gray.(0.8 .* Gray.(img) .> 0.7);
 img_morphograd = morphogradient(Gray.(0.8 * Gray.(img) .> 0.4))
 mosaicview(img_gray, img_morphograd; nrow = 1)
 
@@ -124,9 +136,12 @@ mosaicview(img_gray, img_morphograd; nrow = 1)
 # the external gradient. In `morpholaplace(img, [region])`, `region` allows
 # you to control the dimensions over which this operation is performed.
 
-img_gray = Gray.(0.8 * Gray.(img) .> 0.7);
+img_gray = Gray.(0.8 .* Gray.(img) .> 0.7);
 img_morpholap = morpholaplace(img_gray)
 mosaicview(img_gray, img_morpholap; nrow = 1)
 
 
 save("assets/morphology.gif", cat(img, img_erosion1, img_dilate1, img_opening1, img_closing1, img_bothat1; dims=3); fps=2) #src
+
+# ## Reference
+# -  Basic morphological operations in HIPR developed by Dr. R. Fisher et al : [link](https://homepages.inf.ed.ac.uk/rbf/HIPR2/morops.htm)
