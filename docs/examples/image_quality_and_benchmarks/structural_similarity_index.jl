772fafbe6597c05fcf420cff920751fabf38e944
Ashwani Rathee <ab669522@gmail.com>
2022-12-11 11:33:38 +0545

    cleaned up for better understanding

--- docs/examples/image_quality_and_benchmarks/structural_similarity_index.jl
+++ docs/examples/image_quality_and_benchmarks/structural_similarity_index.jl
@@ -79,31 +79,39 @@ assess(iqi, img_orig, img_const)
 
 # # Peak signal-to-noise ratio
 
-# Peak signal-to-noise ratio (PSNR) is the ratio between the maximum possible power of a 
-# signal and the power of corrupting noise that affects the fidelity of its representation. 
-# PSNR is most easily defined via the [[mean squared error]] (''MSE''). Given a noise-free ''m''×''n''
-#  monochrome image ''I'' and its noisy approximation ''K'', ''MSE'' is defined as:
+# Peak signal-to-noise ratio (PSNR) is used to measure the quality of image in 
+# present of noise and corruption.
+# It is the ratio between the maximum possible power of a signal and the power 
+# of corrupting noise that affects representation fidelity. 
+
+# Given a noise-free m×n monochrome image I and corresponding noise image K. 
+# 'MSE' is defined as:
 
 # $\mathit{MSE} = \frac{1}{m\,n}\sum_{i=0}^{m-1}\sum_{j=0}^{n-1} [I(i,j) - K(i,j)]^2$
 
-# The PSNR (in [[decibel|dB]]) is defined as:
+# The PSNR (in dB) is defined as:
 
 # $\begin{align}\mathit{PSNR} &= 10 \cdot \log_{10} \left( \frac{\mathit{MAX}_I^2}{\mathit{MSE}} \right)\\ &= 20 \cdot \log_{10} \left( \frac{\mathit{MAX}_I}{\sqrt{\mathit{MSE}}} \right)\\ &= 20 \cdot \log_{10} \left( {\mathit{MAX}_I} \right) - 10 \cdot \log_{10} \left( {{\mathit{MSE}}} \right)\end{align}$
 
-# Here, $MAX_I$ is the maximum possible pixel value of the image. When the pixels 
-# are represented using 8 bits per sample, this is 255. More generally, when samples are
-# represented using linear [[Pulse-code modulation|PCM]] with ''B'' bits per sample, $MAX_I$ is $2^b−1$.
+# Here, $MAX_I$ or peakval is the maximum possible pixel value of the image which can be provided
+# in `assess_psnr` function like `assess_psnr(K, I, [1.0])` for monochrome images
+# or `assess_psnr(K, I, [1.0,1.0,1.0])` for multi channel images. 
 
-assess_psnr(img_noise, img_orig) # 13.979400086720483
+peakval = maximum(img_orig) .|> Float64 # peakval is max pixel value in original image
+assess_psnr(img_noise, img_orig, [peakval]) # 13.979400086720483
 
-# Let's change the contents of the noisy image
+# Let's change the contents of the noisy image before applying PSNR. Note that peakval
+# is not required to be passed as it can be automatically calculated too.
 
 noise = ones(size(img_orig)) .* 0.2 .* (maximum(img_orig) - minimum(img_orig))
 mask = rand(Float64, size(img_orig)) .< 0.5
 noise[mask] = noise[mask] .* -3
 img_noise = img_orig + noise
 
-assess_psnr(img_noise, img_orig) # 6.996517750410234
+assess_psnr(img_noise, img_orig)
+
+# Generally, for non-gray image `K`, PSNR is reported against each channel of
+# `I` and outputs a `Vector`, `peakval` needs to be a vector as well.
 
 # ### References
 # 1. Zhou Wang; Bovik, A.C.; ,”Mean squared error: Love it or leave it? A new look at Signal Fidelity Measures,” Signal Processing Magazine, IEEE, vol. 26, no. 1, pp. 98-117, Jan. 2009.
